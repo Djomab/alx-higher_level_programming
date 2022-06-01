5884de2bd72d894ff54ee45220bb337afd914ac8
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-01 12:44:18 -0700

    11. a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    if b >= 0:
-        for  i in range(b, 0, -1):
-            result = result * a
-    else:
-        for i in range(0, (-1 * b)):
-            result = result * (1 / a)
-    return result
+    return (a ** b)
