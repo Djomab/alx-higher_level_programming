26d94cd9bc3c17636f9788ab2b756307d32c9006
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-01 13:27:09 -0700

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,13 @@
 
 def fizzbuzz():
     num = 1
-    while num < 101:
-        if (num % 3 == 0):
+    while num < 101: 
+        if (num % 3 == 0 and num % 5 == 0):
+            print("Fizzbuzz", end='')
+        elif (num % 3 == 0):
             print("Fizz", end='')
         elif (num % 5 == 0):
             print("Buzz", end='')
-        elif (num % 3 == 0 and num % 5 == 0):
-            print("Fizzbuzz", end='')
         else:
             print("{}".format(num), end='')
         if (num != 100):
