20685c68636f74d3436701e7e2cb24d9b96f2c70
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-02 10:04:31 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-nbr = len(sys.argv)
-if nbr == 1:
-    print("0 arguments.")
-elif nbr == 2:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(nbr - 1))
-    for i in range(nbr - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    nbr = len(sys.argv)
+    if nbr == 1:
+        print("0 arguments.")
+    elif nbr == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(nbr - 1))
+        for i in range(nbr - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
