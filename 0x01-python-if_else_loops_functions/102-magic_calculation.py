dd2d52bd36529f1cc1cc54726ee40f492db773fc
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-02 02:54:05 -0700

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
-    return (a ** b + 98) 
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b - c)
