416da8ac0293bf7b21b5d59167e191f52aaa417c
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-05 11:54:48 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for ligne in matrix:
         for elt in ligne:
-            if ligne.index(elt) == len(ligne):
-                print("{:d}".format(elt), end='')
-            print("{:d}".format(elt), end=' ')
+            print("{:d}".format(elt), end='')
+            if elt != ligne[-1]:
+                print(end=' ')
         print()
