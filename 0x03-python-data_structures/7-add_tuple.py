b4ce0022e61fe437905704fef0ade0546ff1602d
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-07 03:17:24 -0700

    bug in 7-add_tuple.py fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,4 +9,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
+
+    if len(tuple_a) > 2 and len(tuple_b) > 2:
+        tuple_a = (tuple_a[0], tuple_a[1])
+         tuple_b = (tuple_b[0], tuple_b[1])
     return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
