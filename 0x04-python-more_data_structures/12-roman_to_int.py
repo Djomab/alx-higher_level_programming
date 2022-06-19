6797324cfd57ac909c1ad1666b5bb9ab41d24ac1
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-19 09:40:15 +0000

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,9 @@
 def roman_to_int(roman_string):
     entier = 0
     i = 0
-    r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+    r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+        'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
+        'CD': 400, 'CM': 900}
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
