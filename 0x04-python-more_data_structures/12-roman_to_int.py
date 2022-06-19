dd8996fef3e01cc3a36a44f65d0f9f78c915acc7
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-19 08:41:11 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,9 @@ def roman_to_int(roman_string):
     i = 0
     r = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
 
+    if not roman_string:
+        return 0
+
     while i < len(roman_string):
         if i+1 < len(roman_string) and roman_string[i:i+2] in r:
             entier += r[roman_string[i:i+2]]
