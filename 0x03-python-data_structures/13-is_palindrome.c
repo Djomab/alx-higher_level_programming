7aa76f0daecdf6fd4eaaf5fa1464e84fc27ca420
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-22 13:33:20 -0700

    tranking untracked files

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: linked list
+ * Return: 0 or 1
+ */
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return (1);
+
+}
