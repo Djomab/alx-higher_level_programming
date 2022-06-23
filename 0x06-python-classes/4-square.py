b9aaf8ab9c98dabdf01e5107b16e9e16556529a0
DJOGONA Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-23 06:35:05 +0100

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ Define a square """
+    def __init__(self, size=0):
+        """
+        Constructeur
+        Args:
+            size(int): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """get square size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set square size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
