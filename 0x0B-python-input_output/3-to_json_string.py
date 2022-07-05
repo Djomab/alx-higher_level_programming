f94b84086fbc0552d0151f5b2d654a0c51a5d881
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-05 14:24:55 +0100

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Contains function that returns JSON representation of an object"""
+import json
+
+def to_json_string(my_obj):
+    """Return JSON rep of my_obj
+    Args:
+        my_obj: an object
+    Return:
+        JSON rep of my_obj
+    """
+    return json.dumps(my_obj)
