f4b03c931ab60d25a6423fb8571cd8ca5e7001ad
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-05 13:37:17 +0100

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 
-
 def write_file(filename="", text=""):
     """ Writes a string to a text file (UTF8)
     Args:
@@ -12,4 +11,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
-
