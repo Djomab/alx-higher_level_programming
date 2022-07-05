cb3b5d6df3e40e16c124479d06293146b1e7b4ec
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-05 13:15:42 +0100

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
             print(f.read(), end="")
