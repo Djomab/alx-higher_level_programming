adf1f8b995727c40f3250bd4a27bd4e8154d8452
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-05 13:51:56 +0100

    removing blank spaces

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+""" Defines a function that writes to file """
+
 
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
-            print(f.read(), end="")
+        print(f.read(), end="")
