cb347a83c23999ee5b233b0cfb0ebf13b5a4b280
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-06 16:28:42 +0100

    solving multuple include

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,7 @@
 import sys
 from os.path import exists
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
