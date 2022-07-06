667d9b29349b9f0f714e102a6f0606ae2799d82e
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-06 22:29:42 +0100

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-   """returns the dictionary description with simple data structure"""
-   return obj.__dict__
+    """returns the dictionary description with simple data structure"""
+    return obj.__dict__
