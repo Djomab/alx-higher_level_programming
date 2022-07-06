8c77f1ef2222db42c8a060a77aab453293de0be2
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-06 16:16:27 +0100

    7. Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Reading all agruments and adding them to list"""
+import sys
+from os.path import exists
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+file = "add_item.json"
+args = sys.argv
+
+if exists(file):
+    an_obj = load_from_json_file(file)
+    for i in range(1, len(args)):
+        an_obj.append(args[i])
+    save_to_json_file(an_obj, file)
+else:
+    save_to_json_file(args[1:], file)
