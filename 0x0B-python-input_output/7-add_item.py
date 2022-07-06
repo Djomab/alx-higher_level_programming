d9e587ea19e37e72aed7ef2406cc28fcae14c244
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-06 16:55:31 +0100

    removing unused import

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
 """Reading all agruments and adding them to list"""
 import sys
 
-
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
