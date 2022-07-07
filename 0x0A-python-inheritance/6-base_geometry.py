d7cb7e8cb924b982688f2132cb794e78a81af40c
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-07 12:59:49 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A Geometry module"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
