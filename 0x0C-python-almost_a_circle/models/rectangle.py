82241f276c6ec8c5ab3ae5b95f27979e5924db4b
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-07-11 01:19:27 +0100

    4. Area first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,7 @@ class Rectangle(Base):
         elif val < 0:
             raise ValueError("y must be >= 0")
         self.__y = val
+
+    def area(self):
+        """Returns the area value of the Rectangle"""
+        return (self.__width * self.__height)
