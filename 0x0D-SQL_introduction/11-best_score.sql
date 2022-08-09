1b89a85f1dcf66af3fc270be1fee09584f07f9ce
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-08-09 20:59:40 +0100

    batch 10-14

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- lists all records with a score >= 10 of the table second_table
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC
