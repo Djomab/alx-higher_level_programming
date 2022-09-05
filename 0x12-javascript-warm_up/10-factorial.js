3b54974a76fd5e00e3f0fdf15f434f43dcf9211a
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-05 20:33:51 +0100

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/**
+ * computes and prints a factorial
+ * The first argument is integer
+ * Factorial of NaN is 1
+ */
+const firstArg = parseInt(process.argv[2], 10);
+
+function factorial (n) {
+  if (isNaN(n) || n < 2) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(firstArg));
+ 
