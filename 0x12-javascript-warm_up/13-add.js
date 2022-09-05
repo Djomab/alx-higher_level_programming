ee904cfc71c42495a4d94d75b95634c7882ac116
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-05 22:54:50 +0100

    13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * returns the addition of 2 integers
+ * The function must be visible from outside
+ * The name of the function must be add
+ */
+ 
+exports.add = function (a, b) {
+  return a + b;
+}
