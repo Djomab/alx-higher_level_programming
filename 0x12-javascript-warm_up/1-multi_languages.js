83acf12287fd9a09121d6cdbf3e5540bc9ba492a
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-05 08:29:09 +0100

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,14 @@
+/*
+ * prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ */
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'JavaScript is amazing';
+
+console.log(c);
+console.log(python);
+console.log(js);
