dd0e01ae73ddda256fcf676d790a696b192c7335
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-05 17:45:15 +0100

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * prints My number: <first argument converted in integer>
+ * if the first argument can be converted to an integer:
+ * else print “Not a number”
+ */
+ const num = parseInt(process.argv[2], 10);
+
+ if (isNaN(num)) {
+  console.log('Not a number');
+ } else {
+  console.log(`My number: ${num}`);
+ }
+ 
