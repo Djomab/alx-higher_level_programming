78f25f116664ed08b0de47e2408236a20a51e239
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-06 12:02:46 +0100

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * class Rectangle
+ * defines a rectangle
+ */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
