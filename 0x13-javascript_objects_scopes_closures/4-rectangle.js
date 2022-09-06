e569be1295f4c026cfe543ae70615d7264f1136d
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-06 13:03:35 +0100

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+/**
+ * class Rectangle that defines a rectangle
+ */
+module.exports = class Rectangle {
+  /**
+   * Attributes
+   *  width: with of rectangle
+   *  height: height of rect
+   *  If w or h is equal to 0 or not a positive integer,
+   *  create an empty object
+   */
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  //prints the rectangle using the character X
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  // exchanges the width and the height of the rectangle
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
