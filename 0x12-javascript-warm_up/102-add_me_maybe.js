1b904ed8efacbe50c88ea28f23c57047aff84d17
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-06 11:06:48 +0100

    102-add_me_maybe.js

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * Executes x times a function
+ * The function must be visible from outside
+ * Prototype: function (x, theFunction)
+ */
+
+ exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};
