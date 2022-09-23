6988ce2333d57338599d9a8af3f842dc3f3db691
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-23 17:50:18 +0100

    8-json_api.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 script that takes in a URL,
-sends a POST request to the passed URL with the email as a parameter,
+sends a request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
 import requests
