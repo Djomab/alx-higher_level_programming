1fea36c80f9a7c9d4f90592bec16e1b4537f9642
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-23 11:46:50 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as request
+
+
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    data = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data.decode('utf-8')))
