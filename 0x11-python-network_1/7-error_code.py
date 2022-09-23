3a034053e69724cc4b4ba617b88ce194f8a02c6f
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-23 17:13:16 +0100

    add test de 17h12

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.post(argv[1])
+    if req.status_code >= 400:
+        print('Error code:'.format(req.status_code))
+    else:
+        print(req.text)
