7b0ba77cd5ff05c2b8b3485e83676f0bb199ece5
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-25 06:13:08 +0100

    updated 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,8 @@ import requests
 
 
 if __name__ == "__main__":
-    data = requests.get('https://intranet.hbtn.io/status')
+    url = 'https://intranet.hbtn.io/status'
+    data = requests.get(url)
     print('Body response:')
     print('\t- type: {}'.format(type(data.text)))
     print('\t- content: {}'.format(data.text))
