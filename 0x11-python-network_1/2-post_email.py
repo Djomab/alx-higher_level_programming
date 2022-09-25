60b7dcddc8250d4bb4bba34fd2525e1acd92b24e
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-09-25 06:07:26 +0100

    updated 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,6 +4,7 @@ script that takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
+from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import sys
 
