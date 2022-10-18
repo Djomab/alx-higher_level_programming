bd52a017ed1322f482206b2b9fe133660859c9d6
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-10-18 13:07:03 +0100

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * display the status code of a GET request.
+ * status code must be printed like this: code: <status code>
+ */
+
+ const request = require('request');
+ const url = process.argv[2];
+
+request (url, function (error, response) {
+  if (!error) {
+    console.log('code: '+response.statusCode);
+  }
+});
