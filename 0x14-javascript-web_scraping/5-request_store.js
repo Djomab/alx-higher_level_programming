4db3af6ed6ca488eada76c9ab254e060d7f9e3a3
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-10-18 14:16:00 +0100

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/**
+ * gets the contents of a webpage and stores it in a file.
+ * The first argument is the URL to request
+ * The second argument the file path to store the body response
+ * The file must be UTF-8 encoded
+ */
+
+const fs = require('fs');
+const request = require('request');
+const filePath = process.argv[3];
+const endpoint = process.argv[2];
+
+request(endpoint, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    reader = fs.writeFile(filePath, body, 'utf-8', function (error) {
+      if (error) console.log(error);
+    });
+  }
+});
