0af8adfb858e55e19608692701f2d231a872b642
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-10-18 14:49:51 +0100

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/**
+ * prints all characters of a Star Wars movie
+ * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+ * Display one character name by line
+ */
+
+const request = require('request');
+const id = process.argv[2];
+const endpoint = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+request(endpoint, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(char => {
+      request(char, function (err, resp, bod) {
+        if (err) console.log(err);
+        else {
+          console.log(JSON.parse(bod).name);
+        }
+      });
+    });
+  }
+});
