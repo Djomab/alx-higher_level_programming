90b3c9092cf0cdd443ea159ae18e7859a7d36ad7
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-10-18 06:28:20 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-/** 
+/**
  * reads and prints the content of a file.
  * first argument is the file path
  * content of the file must be read in utf-8
  */
 
 const fs = require('fs');
-file = process.argv[2];
-fs.readFile(file, 'utf8', function(err, data) {
+const file = process.argv[2];
+fs.readFile(file, 'utf8', function (err, data) {
   console.log(data);
   if (err) throw err;
 });
